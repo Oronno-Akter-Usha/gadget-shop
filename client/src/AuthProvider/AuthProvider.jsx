c0b62206501c2d4ff6837774a1b5cabffbf95633
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-11-16 18:56:04 +0600

    feat: AuthProvider

---
+++ client/src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { createContext, useEffect, useState } from "react";
+import { app } from "./../firebase-config/firebase";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
+  const CreateUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const Login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const Logout = () => {
+    return signOut(auth);
+  };
+  const GoogleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return (
+      () => {
+        return unsubscribe();
+      },
+      []
+    );
+  });
+  const authInfo = {
+    user,
+    loading,
+    CreateUser,
+    Login,
+    Logout,
+    GoogleLogin,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
