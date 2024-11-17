bf18a16421aa32a0bf922032311fd39a2970a600
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-11-17 23:19:15 +0600

    update: integrate React Hook Form into existing Login  page

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,6 +1,19 @@
+/* eslint-disable no-unused-vars */
 import { Link } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const { Login } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -13,7 +26,7 @@ const Login = () => {
           </p>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -22,8 +35,13 @@ const Login = () => {
                 type="email"
                 placeholder="email"
                 className="input input-bordered"
-                required
+                {...register("email", { required: true })}
               />
+              {errors.email && (
+                <p className="text-red-500 text-sm font-light">
+                  Email is required
+                </p>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
@@ -33,16 +51,23 @@ const Login = () => {
                 type="password"
                 placeholder="password"
                 className="input input-bordered"
-                required
+                {...register("password", { required: true, minLength: 6 })}
               />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
+              {errors.password?.type === "required" && (
+                <p className="text-red-500 text-sm font-light">
+                  Password is required
+                </p>
+              )}
+              {errors.password?.type === "minLength" && (
+                <p className="text-red-500 text-sm font-light">
+                  Password must have at least 6 characters
+                </p>
+              )}
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
             </div>
             <p className="my-4 text-sm font-light">
               New here?
