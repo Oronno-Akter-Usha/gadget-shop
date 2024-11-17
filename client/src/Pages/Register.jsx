28f6ebe3a7b6b9966b5f02e3d66d093384406402
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-11-17 23:08:03 +0600

    update: integrate React Hook Form into existing Register page

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,6 +1,20 @@
+/* eslint-disable no-unused-vars */
 import { Link } from "react-router-dom";
-
+import useAuth from "../hooks/useAuth";
+import { useForm } from "react-hook-form";
 const Register = () => {
+  const { CreateUser } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -13,7 +27,7 @@ const Register = () => {
           </p>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -22,8 +36,13 @@ const Register = () => {
                 type="email"
                 placeholder="email"
                 className="input input-bordered"
-                required
+                {...register("email", { required: true })}
               />
+              {errors.email && (
+                <p className="text-red-500 text-sm font-light">
+                  Email is required
+                </p>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
@@ -33,16 +52,46 @@ const Register = () => {
                 type="password"
                 placeholder="password"
                 className="input input-bordered"
-                required
+                {...register("password", { required: true, minLength: 6 })}
               />
+              {errors.password?.type === "required" && (
+                <p className="text-red-500 text-sm font-light">
+                  Password is required
+                </p>
+              )}
+              {errors.password?.type === "minLength" && (
+                <p className="text-red-500 text-sm font-light">
+                  Password must have at least 6 characters
+                </p>
+              )}
+            </div>
+            <div className="form-control">
               <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
+                <span className="label-text">Confirm Password</span>
               </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                {...register("confirmPassword", {
+                  required: true,
+                  validate: (value) => {
+                    if (watch("password") != value) {
+                      return "You password do not match";
+                    }
+                  },
+                })}
+              />
+              {errors.confirmPassword && (
+                <p className="text-red-500 tesm font-light">
+                  Both password must match
+                </p>
+              )}
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Register</button>
+              <button type="submit" className="btn btn-primary">
+                Register
+              </button>
             </div>
             <p className="my-4 text-sm font-light">
               Already have an account?
